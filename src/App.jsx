e5901a9edfe9a8203d93d4041fe5becf33a15b85
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2025-09-30 11:44:06 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+   <main className="overflow-y-hidden  text-neutral-200 ">
+    
+    <HeroSection />
+    <Navbar />
+    
+    
+   </main>
+  )
+}
+
+export default App;
---
+++ src/constants/index.jsx
@@ -0,0 +1,12 @@
+
+
+export const LINKS = [
+  { text: "Jewellery", targetId: "Jewellery" },
+  { text: "About", targetId: "about" },
+  { text: "Mission", targetId: "mission" },
+  { text: "Expertise", targetId: "expertise" },
+  { text: "Review", targetId: "review" },
+  { text: "Contact", targetId: "contact" },
+];
+
+
